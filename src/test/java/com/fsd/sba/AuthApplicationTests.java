2a7474cae23bb70391f1ab3fa971a901ce5d376b
LIANG XIONG <xionglwh@cn.ibm.com>
2019-11-07 10:04:10 +0800

    update.

--- src/test/java/com/fsd/sba/AuthApplicationTests.java
+++ src/test/java/com/fsd/sba/AuthApplicationTests.java
@@ -2,13 +2,17 @@ package com.fsd.sba;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class AuthApplicationTests {
 	@Test
-	public void contextLoads() {
+	public void test() throws Exception {
+
 	}
 }
